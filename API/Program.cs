a749e8ef5d83104a332bec222650246a3ad64881
AvigailShlomov <anm798@gmail.com>
2023-01-31 17:52:37 +0200

    Stage3 - Angular

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,7 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); 
 
 // Add services to the container.
 
@@ -11,10 +11,14 @@ builder.Services.AddDbContext<DataContext>(opt=>
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
